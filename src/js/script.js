3c5e3622e264ccd95cc33c29481e39fd825fe809
Gaël Poupard <gael.poupard@orange.com>
2019-09-05 15:16:01 +0200

    feat: update grids and handle modifications, saved in localStorage

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,33 @@
 (function () {
   'use strict';
 
-  /*document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('[type="number"]').forEach(
-      function(item){
-        var axis  = (item.getAttribute('max') === '16') ? '--posY' : '--posX';
-        var value = localStorage.getItem(item.id);
-        item.parentNode.parentNode.parentNode.style.setProperty(axis, value);
-        item.value = value;
+  const room = document.getElementById('main');
+  const rows = room.style.getPropertyValue('--rows');
+  const cols = room.style.getPropertyValue('--cols');
+  const inputs = document.querySelectorAll('[type="number"]');
+
+  document.addEventListener('DOMContentLoaded', () => {
+    inputs.forEach(
+      item => {
+        const parent = item.parentNode.parentNode.parentNode;
+        const max    = item.getAttribute('max');
+        const axis   = (max === rows) ? '--posY' : '--posX';
+        const value  = localStorage.getItem(item.id);
+        if (value !== null) {
+          parent.style.setProperty(axis, value);
+          item.value = value;
+        }
+
+        item.addEventListener('input', event => {
+            parent.style.setProperty(axis, event.target.value);
+            localStorage.setItem(item.id, event.target.value);
+        });
       });
   });
 
-  document.querySelectorAll('[type="number"]').forEach(
-    function(item, i){
-      item.addEventListener('input', function(event) {
-          var axis = (item.getAttribute('max') === '16') ? '--posY' : '--posX';
-          item.parentNode.parentNode.parentNode.style.setProperty(axis, event.target.value);
-          localStorage.setItem(item.id, event.target.value);
-      });
-    });*/
-
-  document.querySelector('a[download]').addEventListener('click', function() {
+  /*document.querySelector('a[download]').addEventListener('click', function() {
     this.href = 'data:text/json,' + JSON.stringify(localStorage);
-  });
+  });*/
 
   /*document.querySelector('input[type="file"]').addEventListener('change', function(e) {
     var file   = e.target.files[0];
@@ -48,4 +53,6 @@
 
     reader.readAsText(file);
   });*/
+
+  /*document.querySelector('input[type="reset"]') → vider localStorage*/
 })();
